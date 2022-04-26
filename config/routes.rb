cc0afd8432e7ab2662f41db771509e5ec50470c8
bananvyhe <loadonden@yahoo.com>
2022-04-26 14:26:27 +0500

    routes. cors add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-		root to: 'welcome#index'
+	root to: 'welcome#index'
+	post 'refresh', controller: :refresh, action: :create
+	post 'signin', controller: :signin, action: :create
+	post 'signup', controller: :signup, action: :create
+	delete 'signin', controller: :signin, action: :destroy
+
+  resources :todos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
