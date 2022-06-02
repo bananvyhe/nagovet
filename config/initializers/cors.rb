1b011a2d47dbba007b4a16496b445564342373fa
bananvyhe <loadonden@yahoo.com>
2022-06-02 20:33:16 +0500

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,25 +1,10 @@
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#  allow do
-#    origins 'localhost:3000' 
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+ allow do
+   origins 'nagovets.ru' 
 
-#    resource '*',
-#      headers: :any,
-#      credentials: true,
-#      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#  end
-# end
-Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
-  allow do
-    origins '*'
-
-    resource '/cors',
-      :headers => :any,
-      :methods => [:post],
-      :max_age => 0
-
-    resource '*',
-      :headers => :any,
-      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-      :max_age => 0
-  end
+   resource '*',
+     headers: :any,
+     credentials: true,
+     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+ end
 end
