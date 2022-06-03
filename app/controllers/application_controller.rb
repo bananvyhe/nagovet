9fabfe234b35359424b833cba5317c5810404fd2
bananvyhe <loadonden@yahoo.com>
2022-06-03 13:09:01 +0500

    net::ERR_SSL_PROTOCOL_ERROR axios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	# before_action :set_default_format
 	# protect_from_forgery with: :null_session
 	# skip_forgery_protection
-	protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+	# protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 	# protect_from_forgery with: :exception
 	include JWTSessions::RailsAuthorization
   rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
