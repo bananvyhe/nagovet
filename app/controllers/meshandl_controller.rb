9e43ca607afbe8f55b747dd1f6986a5cc01da64b
bananvyhe <loadonden@yahoo.com>
2022-08-12 03:07:36 +0500

    fix

--- app/controllers/meshandl_controller.rb
+++ app/controllers/meshandl_controller.rb
@@ -17,33 +17,20 @@ class MeshandlController < ApplicationController
   		else
   			mass << value.to_s
   		end
-  		# puts mass
-  		# puts index.inspect
-  		# puts value.inspect
+ 
 		end
 
  		mesa = 'имя: '+mass[0]+' тел: '+mass[1]+' сообщение: '+mass[2]+' дата: '+mass[3]+' время: '+mass[4]
  
- 		mesa = 'https://api.telegram.org/bot5531512315:AAHmGCiQQQfdVzQrcja0c9woh5TxrrYldk8/sendMessage?chat_id=199874565&text='+mesa 
- 		# mesa = mesa.to_s
+ 		mesa = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id=199874565&text='+mesa 
+ 
 	 	paf = URI::Parser.new
 	 	paf = paf.escape(mesa)
 	 	puts paf
- 		# +' date: '+ params[:date1].to_s+' time: '+ params[:date2].to_s
+ 
 
 		HTTParty.post(paf)
-
-
-		# puts"=============-===========-=========="
-		# puts token
-		# puts"=============-===========-=========="
-
-	 # 	Telegram::Bot::Client.run(token) do |bot|
-	 # 		 bot.listen do |message|
-  #         puts message
-  #       end
-	 # 		bot.api.send_message(chat_id: "199874565", text: "Hello,sdad")
-		# end
+ 
 	end
 
   private
