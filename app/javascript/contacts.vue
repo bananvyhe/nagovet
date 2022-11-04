515fbf6208e4c158c3f7236f3f7a80e172f91826
bananvyhe <loadonden@yahoo.com>
2022-11-04 15:18:34 +0500

    contacts from db

--- app/javascript/contacts.vue
+++ app/javascript/contacts.vue
@@ -8,9 +8,15 @@
               <div class="d-flex flex-column justify-center align-center pb-4"> 
 
                 <h4>Консультирую:</h4>
-                <div class="body-1 my-2 mb-1">ОНЛАЙН - по видеосвязи (мессенджеры: Scype, WhatsApp, Viber, Telegram)
+                  <div class="desc body-1 my-2 mb-1">                
+                    {{contacts[0].desc}}
+                  </div> 
+                  <div class="body-1 mt-0 mb-3">
+                    {{contacts[1].desc}}
+                  </div>
+<!--                 <div class="body-1 my-2 mb-1">ОНЛАЙН - по видеосвязи (мессенджеры: Scype, WhatsApp, Viber, Telegram)
                 </div>
-                <div class="body-1 mt-0 mb-3">ОЧНО - в Екатеринбурге по адресу: Тверитина 34, корпус 5.</div>
+                <div class="body-1 mt-0 mb-3">ОЧНО - в Екатеринбурге по адресу: Тверитина 34, корпус 5.</div> -->
  <!-- 
                 <v-expansion-panels>
     <v-expansion-panel>
@@ -23,11 +29,15 @@
           </template>
         </v-expansion-panel-header>
         <v-expansion-panel-content> -->
- <div  style="position:relative;overflow:hidden;width:100%;">
+          <div  style="position:relative;overflow:hidden;width:100%;">
+          <span v-html="contacts[0].position"></span>
+            </div>
+<!--  
                   <a href="https://yandex.ru/maps/54/yekaterinburg/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Екатеринбург</a>
                   <a href="https://yandex.ru/maps/54/yekaterinburg/house/ulitsa_tveritina_34_5/YkkYcAVlQEYEQFtsfXRzc3lgbA==/?ll=60.625933%2C56.822538&source=wizgeo&utm_medium=mapframe&utm_source=maps&z=17" style="color:#eee;font-size:12px;position:absolute;top:14px;">Улица Тверитина, 34/5 — Яндекс Карты</a>
                   <iframe  src="https://yandex.ru/map-widget/v1/-/CCUB6JWA9A"  height="600" frameborder="1" allowfullscreen="true" style="position:relative;width:100%;"></iframe>
-                </div>
+                </div> -->
+
      <!--    </v-expansion-panel-content>
       </v-expansion-panel> </v-expansion-panels>
  -->
@@ -45,13 +55,14 @@ export default {
  
   data: function () {
     return {
-      prices: []  
+      contacts: [],
     }
   },
   created () {
     this.$http.plain.get('/contacts')
       .then(response => { 
-        this.prices = response.data 
+        this.contacts = response.data 
+        console.log(response.data )
       })
       .catch(error => { this.setError(error, 'Something went wrong') })
   },   
@@ -59,5 +70,7 @@ export default {
 </script>
 
 <style >
- 
+ .desc{
+  max-width: 800px;
+ }
 </style>
