3b4ab70c6c993e116768cea65f313fba57c69113
bananvyhe <loadonden@yahoo.com>
2022-11-06 18:44:36 +0500

    make request table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_04_074400) do
+ActiveRecord::Schema.define(version: 2022_11_06_133453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 2022_11_04_074400) do
     t.string "zaprosi"
     t.text "education"
     t.text "karta"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.string "position"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
